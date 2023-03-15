ca0fe2bc0c2699ab242c361f3f649eb379e248e4
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-03-15 22:41:26 +0300

    Add DialogFlow learning script.

--- run_dialogflow.py
+++ run_dialogflow.py
@@ -26,6 +26,31 @@ def create_api_key(project_id: str) -> api_keys_v2.Key:
     return response
 
 
+def create_intent(project_id: str, intent_name: str, questions: list, answers: list):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for question in questions:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=question)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=answers)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=intent_name,
+        training_phrases=training_phrases,
+        messages=[message],
+    )
+
+    response = intents_client.create_intent(request={"parent": parent, "intent": intent})
+
+    print("Intent created: {}".format(response))
+
+
 def detect_intent_text(project_id: str, session_id: str, text: str, language_code: str = 'RU-ru') -> str:
     session_client = dialogflow.SessionsClient()
 
