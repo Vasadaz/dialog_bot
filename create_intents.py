e03eaa32c8fea44da1a4a90dffe0d2d994191b0e
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-03-18 21:16:04 +0300

    Reformatted code

--- create_intents.py
+++ create_intents.py
@@ -1,6 +1,6 @@
-import json
+#!/usr/bin/env python
 
-from environs import Env
+import json
 
 from dialogflow import create_intent
 
@@ -9,16 +9,11 @@ INTENTS_FILE_NAME = 'intents.json'
 
 
 if __name__ == '__main__':
-    env = Env()
-    env.read_env()
-    dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
-
     with open(INTENTS_FILE_NAME, 'r') as file:
         intents = json.load(file)
 
     for intent_name, phrases in intents.items():
         create_intent(
-            project_id=dialogflow_project_id,
             intent_name=intent_name,
             questions=phrases['questions'],
             answers=phrases['answers']
